27f6b0962464b8621fddd0753282cb0cbe2bf3db
Ritaban Biswas <48176820+notanoobcoder@users.noreply.github.com>
2020-08-30 20:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const nav = getElements('nav');
-const toggleIcon = getElements('toggle-icon');
-const image1 = getElements('image1');
-const image2 = getElements('image2');
-const image3 = getElements('image3');
-const textBox = getElements('text-box');
+
 const twitteracc = document.getElementById('twitter');
 const githubacc = document.getElementById('github');
 const linkedinacc = document.getElementById('linkedin');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
-// Function to not have to type document.getElementById so much
-function getElements(id) {
-  return document.getElementById(id);
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Function to dynamically change theme
-function modeSwap(theme, navStyle, textStyle, icon1, icon2) {
-  nav.style.backgroundColor = `rgb(${navStyle} ${navStyle} ${navStyle} / 50%)`;
-  textBox.style.backgroundColor = `rgb(${textStyle} ${textStyle} ${textStyle} / 50%)`;
-  toggleIcon.children[1].classList.replace(`fa-${icon1}-o`, `fa-${icon2}-o`);
-  toggleIcon.children[0].textContent = `${theme} Mode`;
-  image1.src = `img/undraw_proud_coder_${theme}.svg`;
-  image2.src = `img/undraw_conceptual_idea_${theme}.svg`;
-  image3.src = `img/undraw_feeling_proud_${theme}.svg`;
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
 }
 
-// Swith theme dynamically
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
+
+// Switch Theme Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    modeSwap('dark', 0, 255, 'sun', 'moon');
+    darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    modeSwap('light', 255, 0, 'moon', 'sun');
+    lightMode();
   }
 }
 
-// Event listener
+// Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
 
-// Check local storage for theme
+// Check Local Storage For Theme
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
+
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
-    modeSwap('dark', 0, 255, 'sun', 'moon');
+    darkMode();
   }
 }
-
 //Tweet Quote
 function twitteraccount() {
     const twitterUrl = `https://twitter.com/perplexed_DNA`;
@@ -58,4 +68,18 @@ function twitteraccount() {
 
 }
 
+function linkedinaccount() {
+  const linkedinUrl = `https://www.linkedin.com/in/ritaban-biswas-80b624181/`;
+  window.open(linkedinUrl, '_blank'); 
+
+}
+
+function githubaccount() {
+  const githubUrl = `https://github.com/notanoobcoder`;
+  window.open(githubUrl, '_blank'); 
+
+}
+
 twitteracc.addEventListener('click',twitteraccount);
+linkedinacc.addEventListener('click',linkedinaccount);
+githubacc.addEventListener('click',githubaccount);
